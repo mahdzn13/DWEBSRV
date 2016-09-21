8ce2d4d8167480ab180aa5eee1186827781d1eb0
mahernandezd <mahdzn13@gmail.com>
2016-09-21 20:55:02 +0200

    -Added pages.

--- src/Practica1/Agenda.java
+++ src/Practica1/Agenda.java
@@ -1,11 +1,14 @@
 package Practica1;
 
+import java.util.List;
+
 /**
  * Created by mahernandezd on 21/09/16.
  */
 
 public class Agenda {
     private int any;
+    private List<Pagina> pagines;
 
 
     public void avPag(){
@@ -22,6 +25,14 @@ public class Agenda {
 
     public void setAny(int any) {
         this.any = any;
+    }
+
+    public List<Pagina> getPagines() {
+        return pagines;
+    }
+
+    public void setPagines(List<Pagina> pagines) {
+        this.pagines = pagines;
     }
 }
 
